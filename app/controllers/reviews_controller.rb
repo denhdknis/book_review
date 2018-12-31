fc4cb38b9470bec843e1e7bf6b51653b61ec4ae5
田　英毅 <denhdk1997@yahoo.co.jp>
2018-12-31 18:21:25 +0900

    review投稿機能追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,18 @@
 class ReviewsController < ApplicationController
 
   def new
-  	@product = Product.find([:product.id])
+  	@product = Product.find([:product_id])
   	@review = Review.new
   end
 
   def create
-  	@review = Review.new
-  	@review.create(review_params)
+  	@review = Review.create(review_params)
+  	redirect_to :controller :products, :action :index
   end
 
 
   private
    def review_params
-   	 params.require(:product).permit(:rate,:review)
+   	 params.require(:product).permit(:rate,:review).marge(:product_id,params[:product_id])
    end
-
 end
