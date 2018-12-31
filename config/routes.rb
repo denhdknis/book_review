45edb9f1089f502519baafd433869fba9af19335
田　英毅 <denhdk1997@yahoo.co.jp>
2018-12-31 19:08:57 +0900

    ルーティングの細かい修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :products, only: :show
+  resources :products, only: :show do 
   resources :reviews , only: [:create,:new]
     collection do
 	   get :search
